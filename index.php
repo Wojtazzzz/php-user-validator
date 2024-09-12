9f5377b46f799bcce8ecf17d4117936515a1550d
Wojtazz <marcin.witas72@gmail.com>
2024-09-12 19:55:47 +0200

    feat: add validatePassword method

--- index.php
+++ index.php
@@ -8,10 +8,17 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $validator = new UserValidator();
 
-$email = "test@example.com";
+$email = 'test@example.com';
+$password = 'StrongPass1!';
 
 if ($validator->validateEmail($email)) {
 	echo "Email is valid.\n";
 } else {
 	echo "Email is invalid.\n";
+}
+
+if ($validator->validatePassword($password)) {
+	echo "Password is valid.\n";
+} else {
+	echo "Password is invalid.\n";
 }
--- src/UserValidator.php
+++ src/UserValidator.php
@@ -20,4 +20,29 @@ final readonly class UserValidator
 
 		return true;
 	}
+
+	public function validatePassword(string $password): bool
+	{
+		if (strlen($password) < 8) {
+			return false;
+		}
+
+		if (!preg_match('/[A-Z]/', $password)) {
+			return false;
+		}
+
+		if (!preg_match('/[a-z]/', $password)) {
+			return false;
+		}
+
+		if (!preg_match('/[0-9]/', $password)) {
+			return false;
+		}
+
+		if (!preg_match('/\W/', $password)) {
+			return false;
+		}
+
+		return true;
+	}
 }
